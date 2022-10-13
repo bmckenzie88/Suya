d9b985868590a9cc7a9b3438cdfbc7ba5918e32f
Bryce McKenzie <brycemckenzie@Roses-MacBook-Air.local>
2022-10-13 09:35:04 -0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,34 +41,4 @@ button.onclick = function () {
   navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
 };
 
-//============
-var map;
-var service;
-var infowindow;
-
-function initialize() {
-  var spokane = new google.maps.LatLng(-33.8665433,151.1956316);
-
-  map = new google.maps.Map(document.getElementById('map'), {
-      center: spokane,
-      zoom: 15
-    });
-
-  var request = {
-    location: spokane,
-    radius: '500',
-    query: 'restaurant'
-  };
-
-  service = new google.maps.places.PlacesService(map);
-  service.textSearch(request, callback);
-}
-
-function callback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      var place = results[i];
-      createMarker(results[i]);
-    }
-  }
-}
+//============API function=================
